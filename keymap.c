7de02e724377d025ab12046bde503dce14ed084c
Anthony Sellitti <anthony.sellitti@envato.com>
2021-02-02 17:06:02 +1100

    Use the short keycodes to minimize sideway scroll

--- keymap.c
+++ keymap.c
@@ -46,37 +46,102 @@ enum custom_keycodes {
   ST_MACRO_0,
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /*
+   * Keymap: Default Layer 0, QWERTY
+   *
+   * ,--------------------------------------------------.      ,--------------------------------------------------.
+   * |    `   |   1  |   2  |   3  |   4  |   5  |  {   |      |   -  |   6  |   7  |   8  |   9  |   0  |   =    |
+   * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
+   * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  [   |      |  ]   |   Y  |   U  |   I  |   O  |   P  |   '    |
+   * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
+   * |   ESC  |   A  |   S  |   D  |   F  |   G  |------|      |------|   H  |   J  |   K  |   L  |   ;  |   \    |
+   * |--------+------+------+------+------+------|  =>  |      |  :   |------+------+------+------+------+--------|
+   * |  LSFT  |   Z  |   X  |   C  |   V  |   B  |      |      |      |   N  |   M  |   ,  |   .  |   /  |  RSFT  |
+   * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
+   *   |  ~L1 |      | LEFT | RGHT | LCTL |                                  | RCTL |  UP  | DOWN |      | ~L1  |
+   *   `----------------------------------'                                  `----------------------------------'
+   *                                        ,-------------.  ,-------------.
+   *                                        | HOME |  END |  | PGUP | PGDN |
+   *                                 ,------|------|------|  |------+------+------.
+   *                                 |      |      | LAlt |  | RAlt |      |      |
+   *                                 | BSPC | DEL  |------|  |------| ENT  | SPC  |
+   *                                 |      |      | LGUI |  | RGUI |      |      |
+   *                                 `--------------------'  `--------------------'
+   */
   [0] = LAYOUT_ergodox_pretty(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LCBR,                                        KC_MINUS,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_EQUAL,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_LBRACKET,                                    KC_RBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_QUOTE,
-    KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_BSLASH,
-    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           ST_MACRO_0,                                     KC_COLN,        KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
-    MO(1),          KC_TRANSPARENT, KC_LEFT,        KC_RIGHT,       KC_LCTRL,                                                                                                       KC_RCTRL,       KC_UP,          KC_DOWN,        KC_TRANSPARENT, MO(1),
-                                                                                                    KC_HOME,        KC_END,         KC_PGUP,        KC_PGDOWN,
-                                                                                                                    KC_LALT,        KC_RALT,
-                                                                                    KC_BSPACE,      KC_DELETE,      LGUI_T(KC_NO),  RGUI_T(KC_NO),  KC_ENTER,       KC_SPACE
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,      /**/KC_MINS,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,      /**/KC_RBRC,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  /**/               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ST_MACRO_0,   /**/KC_COLN,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    MO(1),   KC_TRNS, KC_LEFT, KC_RGHT, KC_LCTL,                        /**/                        KC_RCTL, KC_UP,   KC_DOWN, KC_TRNS, MO(1),
+                                                 KC_HOME, KC_END,       /**/KC_PGUP,       KC_PGDN,
+                                                          KC_LALT,      /**/KC_RALT,
+                                        KC_BSPC, KC_DEL,  LGUI_T(KC_NO),/**/RGUI_T(KC_NO), KC_ENT,  KC_SPC
   ),
+  /*
+   * Keymap: Layer 1, Arrows, Media Keys
+   *
+   * ,--------------------------------------------------.      ,--------------------------------------------------.
+   * |  Vol+  |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+   * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
+   * |  Vol-  |      |  Up  |      |      |      | Next |      | Prev | MWUP | MUP  | MWDN |      |      |        |
+   * |--------+------+------+------+------+------| Trck |      | Trck |------+------+------+------+------+--------|
+   * |        | Left | Down | Rght |      |      |------|      |------| MLFT | MDWN | MRGT |      |      |  ~L2   |
+   * |--------+------+------+------+------+------| Mute |      | Play |------+------+------+------+------+--------|
+   * |        |      |      |      |      |      |      |      | Paus | LCLK | MCLK | RCLK |      |      |        |
+   * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
+   *   | #### |      |      |      |      |                                  |      |      |      |      | #### |
+   *   `----------------------------------'                                  `----------------------------------'
+   *                                        ,-------------.  ,-------------.
+   *                                        |      |      |  |      |      |
+   *                                 ,------|------|------|  |------+------+------.
+   *                                 |      |      |      |  |      |      |      |
+   *                                 |      |      |------|  |------|      |      |
+   *                                 |      |      |      |  |      |      |      |
+   *                                 `--------------------'  `--------------------'
+   */
   [1] = LAYOUT_ergodox_pretty(
-    KC_AUDIO_VOL_UP,KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
-    KC_AUDIO_VOL_DOWN,KC_TRANSPARENT, KC_UP,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_NEXT_TRACK,                                KC_MEDIA_PREV_TRACK,KC_MS_WH_UP,    KC_MS_UP,       KC_MS_WH_DOWN,  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT, KC_TRANSPARENT, MO(2),
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_MUTE,                                  KC_MEDIA_PLAY_PAUSE,KC_MS_BTN1,     KC_MS_BTN3,     KC_MS_BTN2,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_VOLU, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,/**/KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+    KC_VOLD, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT,/**/KC_MPRV, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,         /**/         KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, MO(2),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,/**/KC_MPLY, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  /**/                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                                 KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS,
+                                                          KC_TRNS,/**/KC_TRNS,
+                                        KC_TRNS, KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS, KC_TRNS
   ),
+  /*
+   * Keymap: Layer 2, Macros
+   *
+   * ,--------------------------------------------------.      ,--------------------------------------------------.
+   * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |        |
+   * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
+   * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |        |
+   * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
+   * |        |      |      |      |      |      |------|      |------|      |      |      |      |      | ###### |
+   * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
+   * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |        |
+   * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
+   *   | #### |      |      |      |      |                                  |      |      |      |      | #### |
+   *   `----------------------------------'                                  `----------------------------------'
+   *                                        ,-------------.  ,-------------.
+   *                                        |      |      |  |      |      |
+   *                                 ,------|------|------|  |------+------+------.
+   *                                 |      |      |      |  |      |      |      |
+   *                                 |      |      |------|  |------|      |      |
+   *                                 |      |      |      |  |      |      |      |
+   *                                 `--------------------'  `--------------------'
+   */
   [2] = LAYOUT_ergodox_pretty(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, RESET,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         /**/         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  /**/                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                                 KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS,
+                                                          KC_TRNS,/**/KC_TRNS,
+                                        KC_TRNS, KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
@@ -86,7 +151,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case ST_MACRO_0:
     if (record->event.pressed) {
       SEND_STRING(SS_TAP(X_EQUAL) SS_DELAY(100) SS_LSFT(SS_TAP(X_DOT)));
-
     }
     break;
   }
