88edf0b3fb26cdfb3e8d78e563ef2d2ab3260c7b
Anthony Sellitti <anthony.sellitti@envato.com>
2021-02-02 17:42:34 +1100

    Move macros into a separate file

--- keymap.c
+++ keymap.c
@@ -24,6 +24,7 @@
 #include "keymap_estonian.h"
 #include "keymap_belgian.h"
 #include "keymap_us_international.h"
+#include "macros.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
@@ -43,7 +44,8 @@
 
 enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
-  ST_MACRO_0,
+  M_ROCKET,
+  M_DEVCC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -73,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,      /**/KC_MINS,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,      /**/KC_RBRC,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  /**/               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ST_MACRO_0,   /**/KC_COLN,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    M_ROCKET,     /**/KC_COLN,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     MO(1),   KC_TRNS, KC_LEFT, KC_RGHT, KC_LCTL,                        /**/                        KC_RCTL, KC_UP,   KC_DOWN, KC_TRNS, MO(1),
                                                  KC_HOME, KC_END,       /**/KC_PGUP,       KC_PGDN,
                                                           KC_LALT,      /**/KC_RALT,
@@ -83,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Keymap: Layer 1, Arrows, Media Keys
    *
    * ,--------------------------------------------------.      ,--------------------------------------------------.
-   * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+   * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |      | DevCC|  F6  |  F7  |  F8  |  F9  |  F10 |        |
    * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
    * |  Vol+  |      |  Up  |      |      |      | Next |      | Prev | MWUP | MUP  | MWDN |      |      |        |
    * |--------+------+------+------+------+------| Trck |      | Trck |------+------+------+------+------+--------|
@@ -102,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                 `--------------------'  `--------------------'
    */
   [1] = LAYOUT_ergodox_pretty(
-    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,/**/KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,/**/M_DEVCC, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
     KC_VOLU, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT,/**/KC_MPRV, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,         /**/         KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, MO(2),
     KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP,/**/KC_MPLY, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -148,11 +150,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case ST_MACRO_0:
-    if (record->event.pressed) {
-      SEND_STRING(SS_TAP(X_EQUAL) SS_DELAY(100) SS_LSFT(SS_TAP(X_DOT)));
-    }
-    break;
+    case M_ROCKET:
+      if (record->event.pressed) MACRO_ROCKET;
+      break;
+    case M_DEVCC:
+      if (record->event.pressed) MACRO_DEV_CREDIT_CARD;
+      break;
   }
   return true;
 }
