879b53e641268bc8d63cf472206776e23f36b3b4
Anthony Sellitti <anthony.sellitti@envato.com>
2021-02-02 17:15:52 +1100

    Remap volume control and fix reset comment

--- keymap.c
+++ keymap.c
@@ -83,13 +83,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Keymap: Layer 1, Arrows, Media Keys
    *
    * ,--------------------------------------------------.      ,--------------------------------------------------.
-   * |  Vol+  |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+   * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |      |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
    * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
-   * |  Vol-  |      |  Up  |      |      |      | Next |      | Prev | MWUP | MUP  | MWDN |      |      |        |
+   * |  Vol+  |      |  Up  |      |      |      | Next |      | Prev | MWUP | MUP  | MWDN |      |      |        |
    * |--------+------+------+------+------+------| Trck |      | Trck |------+------+------+------+------+--------|
-   * |        | Left | Down | Rght |      |      |------|      |------| MLFT | MDWN | MRGT |      |      |  ~L2   |
-   * |--------+------+------+------+------+------| Mute |      | Play |------+------+------+------+------+--------|
-   * |        |      |      |      |      |      |      |      | Paus | LCLK | MCLK | RCLK |      |      |        |
+   * |  Mute  | Left | Down | Rght |      |      |------|      |------| MLFT | MDWN | MRGT |      |      |  ~L2   |
+   * |--------+------+------+------+------+------| Stop |      | Play |------+------+------+------+------+--------|
+   * |  Vol-  |      |      |      |      |      |      |      | Paus | LCLK | MCLK | RCLK |      |      |        |
    * `--------+------+------+------+------+-------------'      `-------------+------+------+------+------+--------'
    *   | #### |      |      |      |      |                                  |      |      |      |      | #### |
    *   `----------------------------------'                                  `----------------------------------'
@@ -102,10 +102,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                 `--------------------'  `--------------------'
    */
   [1] = LAYOUT_ergodox_pretty(
-    KC_VOLU, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,/**/KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-    KC_VOLD, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT,/**/KC_MPRV, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,         /**/         KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, MO(2),
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE,/**/KC_MPLY, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,/**/KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+    KC_VOLU, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT,/**/KC_MPRV, KC_WH_U, KC_MS_U, KC_WH_D, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_MUTE, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,         /**/         KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, MO(2),
+    KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MSTP,/**/KC_MPLY, KC_BTN1, KC_BTN3, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                  /**/                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                  KC_TRNS, KC_TRNS,/**/KC_TRNS, KC_TRNS,
                                                           KC_TRNS,/**/KC_TRNS,
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * Keymap: Layer 2, Macros
    *
    * ,--------------------------------------------------.      ,--------------------------------------------------.
-   * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |        |
+   * |        |      |      |      |      |      |      |      |      |      |      |      |      |      | Reset  |
    * |--------+------+------+------+------+-------------|      |------+------+------+------+------+------+--------|
    * |        |      |      |      |      |      |      |      |      |      |      |      |      |      |        |
    * |--------+------+------+------+------+------|      |      |      |------+------+------+------+------+--------|
